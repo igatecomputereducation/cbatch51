bf59a0587a5c7d73c0a79de08e5582e882015884
igate solutions <igatecomputereducation@gmail.com>
2023-06-15 12:12:21 +0530

    v1

--- demo.c
+++ demo.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,c;
-	a=40<80 || 20<=30 && 60!=20;
-	b=60!=60 && 40<80 || 30>20;
-	c=10==10 && 10==15;
-	printf("%i\n%i\n%i",a,b,c);
+	if(10!=10)
+		printf("one");
+	else if(20<=40)
+		printf("two");
+	else if(40>=40)
+		printf("three");
+	else if(50>=90)
+		printf("four");
+	else
+		printf("hello");
 	return 0;
 }
