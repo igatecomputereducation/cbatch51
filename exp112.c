379a2e1c3ac25d7768f847ba8f350c7dcba5cde8
igate <igatecomputereducation@gmail.com>
2023-07-01 11:55:11 +0530

    v1

---
+++ exp112.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n,rev,temp;
+	printf("Enter any natural number:");
+	scanf("%i",&n);
+	temp=n;
+	for(rev=0;n!=0;n=n/10)
+		rev=rev*10+n%10;
+	if(rev==temp)
+		printf("Palindrome number");
+	else
+		printf("Not a palindrome number");
+	return 0;
+}
